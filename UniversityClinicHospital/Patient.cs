e457b4767925b288c8d39fa8e9f6addcbd65db75
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-02-08 22:48:57 -0500

    Added patient

---
+++ UniversityClinicHospital/Patient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityClinicHospital
+{
+    public class Patient : Employee
+    {
+        public int Blood_Level { get; set; }
+        public int Health_Level { get; set; }
+
+        public Patient()
+        {
+            Blood_Level = 20;
+            Health_Level = 10;
+        }
+
+        public void AddPatient()
+        {
+            Console.WriteLine("What is the patients name?");
+            Name = Console.ReadLine();
+        }              
+    }
+}
